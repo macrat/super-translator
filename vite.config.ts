bb98491d877d1739a3bd9cbf91f46f74ba98fa34
MacRat <m@crat.jp>
2023-07-26 20:27:33 +0900

    perf: set preload header for assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import preload from 'vite-plugin-preload';
 
 export default defineConfig({
-  plugins: [svelte(), wasm(), topLevelAwait()],
+  plugins: [svelte(), wasm(), topLevelAwait(), preload()],
 })
